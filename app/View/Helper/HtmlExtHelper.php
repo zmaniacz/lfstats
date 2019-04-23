8e28207948a60b52601bcea987c4fd5deebda7d3
Russell Lewis <russell.lewis@outlook.com>
2019-04-23 16:53:07 -0700

    changed querystring preservation behavior

--- app/View/Helper/HtmlExtHelper.php
+++ app/View/Helper/HtmlExtHelper.php
@@ -1,20 +1,17 @@
 <?php
-	App::uses('HtmlHelper', 'View/Helper');
-	
-	class HtmlExtHelper extends HtmlHelper {
-		var $helpers = array('Session');
-		
-		public function url($url = null, $full = false) {
-			if(is_array($url) && empty($url['?'])) {
-				$querystring = array('?' => array(
-										'gametype' => $this->Session->read('state.gametype'),
-										'leagueID' => $this->Session->read('state.leagueID'),
-										'centerID' => $this->Session->read('state.centerID'),
-										'isComp' => $this->Session->read('state.isComp')
-				));
-				$url = $url + $querystring;
-			}
+    App::uses('HtmlHelper', 'View/Helper');
+    
+    class HtmlExtHelper extends HtmlHelper
+    {
+        public $helpers = array('Session');
+        
+        public function url($url = null, $full = false)
+        {
+            if (is_array($url) && empty($url['?'])) {
+                $querystring = array('?' => $this->request->query);
+                $url = $url + $querystring;
+            }
 
-			return parent::url($url, $full);
-		}
-	}
+            return parent::url($url, $full);
+        }
+    }
