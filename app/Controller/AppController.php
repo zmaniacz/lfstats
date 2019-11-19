a3d278f462009cabc73cebcc7bd2bc43cc54e19d
Russell Lewis <russell.lewis@outlook.com>
2019-11-19 01:11:09 -0800

    Fixed type comparison for auth

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Application level Controller
+ * Application level Controller.
  *
  * This file is application-wide controller file. You can put all
  * application-wide controller-related methods here.
@@ -15,32 +15,32 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
+ *
+ * @see          http://cakephp.org CakePHP(tm) Project
  * @since         CakePHP(tm) v 0.2.9
+ *
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 App::uses('Controller', 'Controller');
 
 /**
- * Application Controller
+ * Application Controller.
  *
  * Add your application-wide methods in the class below, your controllers
  * will inherit them.
  *
- * @package		app.Controller
- * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ * @see		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller
 {
-    public $helpers = array(
-        'Html' => array(
-            'className' => 'HtmlExt'
-        )
-    );
+    public $helpers = [
+        'Html' => [
+            'className' => 'HtmlExt',
+        ],
+    ];
 
     /**
-     * List of global controller components
+     * List of global controller components.
      *
      * @var array
      */
@@ -48,29 +48,31 @@ class AppController extends Controller
         'RequestHandler',
         'Session',
         'Flash',
-        'Auth' => array(
-            'logoutRedirect' => array(
+        'Auth' => [
+            'logoutRedirect' => [
                 'controller' => 'scorecards',
                 'action' => 'index',
-                'home'
-            ),
-            'authorize' => array('Controller')
-        ),
-        'DebugKit.Toolbar'
+                'home',
+            ],
+            'authorize' => ['Controller'],
+        ],
+        'DebugKit.Toolbar',
     ];
 
-    public $uses = array('Center', 'Event', 'Scorecard', 'Game');
+    public $uses = ['Center', 'Event', 'Scorecard', 'Game'];
 
     public function isAuthorized($user)
     {
-        if (isset($user['role']) && $user['role'] === 'admin') {
-            return true;
-        } elseif (isset($user['role']) && $user['role'] === 'center_admin' && $user['center'] === $this->Session->read('state.centerID')) {
+        if (isset($user['role']) && 'admin' === $user['role']) {
             return true;
         }
+        if (isset($user['role']) && 'center_admin' === $user['role'] && $user['center'] == $this->Session->read('state.centerID')) {
+            return true;
+        }
+
         return false;
     }
-    
+
     public function beforeFilter()
     {
         //read state from the querystring; default to social games at LTC if no state passed
@@ -79,13 +81,13 @@ class AppController extends Controller
         } elseif (!$this->Session->check('state.gametype')) {
             $this->Session->write('state.gametype', 'social');
         }
-        
+
         if (!is_null($this->request->query('centerID'))) {
             $this->Session->write('state.centerID', $this->request->query('centerID'));
         } elseif (!$this->Session->check('state.centerID')) {
             $this->Session->write('state.centerID', 14);
         }
-        
+
         if (!is_null($this->request->query('leagueID'))) {
             $this->Session->write('state.leagueID', $this->request->query('leagueID'));
         } elseif (!$this->Session->check('state.leagueID')) {
@@ -97,7 +99,7 @@ class AppController extends Controller
         } elseif (!$this->Session->check('state.isComp')) {
             $this->Session->write('state.isComp', 0);
         }
-        
+
         if (!is_null($this->request->query('show_rounds'))) {
             $this->Session->write('state.show_rounds', $this->request->query('show_rounds'));
         } elseif (!$this->Session->check('state.show_rounds')) {
@@ -115,19 +117,19 @@ class AppController extends Controller
         } elseif (!$this->Session->check('state.show_subs')) {
             $this->Session->write('state.show_subs', false);
         }
-        
+
         //get a center and league object for use throughout the app
-        if (($this->Session->read('state.gametype') == 'all' || $this->Session->read('state.gametype') == 'social') && $this->Session->read('state.centerID') > 0) {
+        if (('all' == $this->Session->read('state.gametype') || 'social' == $this->Session->read('state.gametype')) && $this->Session->read('state.centerID') > 0) {
             $this->set('selected_center', $this->Center->findById($this->Session->read('state.centerID')));
-        } elseif ($this->Session->read('state.gametype') == 'league' && $this->Session->read('state.leagueID') > 0) {
-            $event = $this->Event->find('first', array(
-                'contain' => array(
-                    'Center'
-                ),
-                'conditions' => array(
-                    'Event.id' => $this->Session->read('state.leagueID')
-                )
-            ));
+        } elseif ('league' == $this->Session->read('state.gametype') && $this->Session->read('state.leagueID') > 0) {
+            $event = $this->Event->find('first', [
+                'contain' => [
+                    'Center',
+                ],
+                'conditions' => [
+                    'Event.id' => $this->Session->read('state.leagueID'),
+                ],
+            ]);
 
             if ($event['Event']['is_comp']) {
                 $this->set('selected_league', $event);
@@ -139,7 +141,7 @@ class AppController extends Controller
                 $this->Session->write('state.isComp', $event['Event']['is_comp']);
             }
         }
-        
+
         $this->set('centers', $this->Center->find('list'));
         $this->set('leagues', $this->Event->getLeagueList());
         $this->set('league_details', $this->Event->getLeagueDetailList());
