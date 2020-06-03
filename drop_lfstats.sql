f8652194a83a88887c0acf1588ed407db87d0838
Russell Lewis <russell.lewis@outlook.com>
2020-06-03 00:50:44 -0700

    Adding another query for argh

---
+++ drop_lfstats.sql
@@ -0,0 +1,5 @@
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'LFSTATS' -- ← change this to your DB
+  AND pid <> pg_backend_pid();
+DROP DATABASE LFSTATS;
