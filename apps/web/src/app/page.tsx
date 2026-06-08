1ac1cdc442ee24a7a360c1054e13e4194963cb53
zmaniacz <russell.lewis@outlook.com>
2026-06-07 17:18:51 -0700

    Quick informational udpate to front page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,6 +5,23 @@ export default function Home() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Welcome to LFstats</h1>
+      <p>
+        Over the next few weeks, you'll see this new version of the site more
+        fully come to life. Any games uploaded to the current lfstats.com will
+        appear here as well. I'm targeting end of June for the full launch, so
+        we'll have time to work out any kinks prior to Internats.
+      </p>
+      <p>
+        You'll note the stats here are limited to the TDF (~2020-present) era.
+        Hammering TDF data into a system designed to read the old PDFs was no
+        longer feasible. The current site will continue as a read-only version
+        until I'm able to migrate the legacy data.
+      </p>
+      <p>
+        For now, enjoy the new site and let me know on Discord what you like or
+        hate. Any site admins, please login to the website with your Googel
+        account so we can get access set up.
+      </p>
     </div>
-  )
+  );
 }
